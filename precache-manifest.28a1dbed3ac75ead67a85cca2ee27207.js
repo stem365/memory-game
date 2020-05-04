8cb0c11c071c9ff1081b53f93985c7974df63a56
Stefan Michetschlager <stefan.michetschlager@gmail.com>
2020-05-04 12:37:28 +0300

    Updates

---
+++ precache-manifest.28a1dbed3ac75ead67a85cca2ee27207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3326d1fb13c2393a9a85d43f68c37f08",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "f0caee11938c725807e0",
+    "url": "/memory-game/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "507b18532e95c56175aa",
+    "url": "/memory-game/static/css/main.2c4df2e1.chunk.css"
+  },
+  {
+    "revision": "f0caee11938c725807e0",
+    "url": "/memory-game/static/js/2.ff98b6d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/memory-game/static/js/2.ff98b6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507b18532e95c56175aa",
+    "url": "/memory-game/static/js/main.ef08db90.chunk.js"
+  },
+  {
+    "revision": "a7295d09a3c7d39f3388",
+    "url": "/memory-game/static/js/runtime-main.cd8959cd.js"
+  }
+]);
